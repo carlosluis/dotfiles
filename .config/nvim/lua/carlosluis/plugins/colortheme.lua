63d97c9513c373d85d1b8a969b7d1de8a8b4d9d4
Carlos Luis <carlos.luisgonc@gmail.com>
2024-10-04 15:07:00 +0200

    Make split view transparent

--- .config/nvim/lua/carlosluis/plugins/colortheme.lua
+++ .config/nvim/lua/carlosluis/plugins/colortheme.lua
@@ -21,6 +21,7 @@ return {
       }
       vim.cmd.colorscheme 'tokyonight-night'
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
       -- You can configure highlights by doing something like:
